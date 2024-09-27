319baf9942e175c4c060e78c055e6fadbe2b145b
Dima <darkstrik2000@gmail.com>
2024-09-27 11:11:03 +0300

    Майже усьо
    майже

--- blog/routes/web.php
+++ blog/routes/web.php
@@ -1,7 +1,15 @@
 <?php
-
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\PostController;
+use App\Http\Controllers\CommentController;
 
+// Головна сторінка
 Route::get('/', function () {
-    return view('welcome');
+    return view('welcome'); 
 });
+
+// Ресурсний маршрут для постів
+Route::resource('posts', PostController::class);
+
+// Маршрут для додавання коментарів
+Route::post('posts/{post}/comments', [CommentController::class, 'store'])->name('comments.store');
